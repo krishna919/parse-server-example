c77b1da795b20416b9deec48a7a096c956beb045
Krishna <34255240+krishna919@users.noreply.github.com>
2018-07-03 18:38:22 +0530

    Display data

--- cloud/main.js
+++ cloud/main.js
@@ -9,7 +9,7 @@ Parse.Cloud.define('hellohi', function(req, res) {
   Parse.Cloud.httpRequest({
     url: 'http://laravel.notevault.com/notevault/sample.php'
   }).then(function(httpResponse) {
-    res.success(httpResponse);
+    res.success(httpResponse.data);
   }, function(httpResponse) {
     res.error('Request failed with response code ' + httpResponse.status);
   });
